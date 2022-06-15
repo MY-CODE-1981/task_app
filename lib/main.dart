dc7643f3d0b3affa3b64e572b6427187b50d3b40
Masanori-Yoshihira <j0115765@jp.honda>
2022-06-16 00:53:55 +0900

    add StatefulWidget of TodoListPage

--- lib/main.dart
+++ lib/main.dart
@@ -9,24 +9,69 @@ class MyTodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // ここは後で消す
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('リスト一覧'),
-        ),
-      )
-
       // 右上に表示される"debug"ラベルを消す
-      // debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       // アプリ名
-      // title: 'My Todo App',
-      // theme: ThemeData(
-      //   // テーマカラー
-      //   primarySwatch: Colors.blue,
-      //   visualDensity: VisualDensity.adaptivePlatformDensity,
-      // ),
+      title: 'My Todo App',
+      theme: ThemeData(
+        // テーマカラー
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       // リスト一覧画面を表示
-      // home: TodoListPage(),
+      home: TodoListPage(),
+    );
+  }
+}
+
+// リスト一覧画面用Widget
+class TodoListPage extends StatefulWidget {
+  @override
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  // Todoリストのデータ
+  List<String> todoList = ['テスト', 'flutter', 'アンドロイド'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // AppBarを表示し、タイトルも設定
+      appBar: AppBar(
+        title: Text('リスト一覧'),
+      ),
+      // データを元にListViewを作成
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(todoList[index]),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+        //   // "push"で新規画面に遷移
+        //   // リスト追加画面から渡される値を受け取る
+        //   final newListText = await Navigator.of(context).push(
+        //     MaterialPageRoute(builder: (context) {
+        //       // 遷移先の画面としてリスト追加画面を指定
+        //       return TodoAddPage();
+        //     }),
+        //   );
+        //   if (newListText != null) {
+        //     // キャンセルした場合は newListText が null となるので注意
+        //     setState(() {
+        //       // リスト追加
+        //       todoList.add(newListText);
+        //     });
+        //   }
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
